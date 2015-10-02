9e9ecd40f28715e25e3b9761a8969c44aa98d47f
Andrew Bowler <ajbowler@iastate.edu>
2015-10-02 00:14:42 -0500

    First pages commit

---
+++ js/voice-a-sketch.js
@@ -0,0 +1,29 @@
+var recognition = new webkitSpeechRecognition();
+
+recognition.continuous = true;
+recognition.lang = "en-US";
+recognition.maxAlternative = 1;
+
+recognition.onresult = function(event) {
+  // make sure it isn't fucked first
+  if (typeof(event.results) == "undefined") {
+    recognition.stop();
+    return;
+  }
+
+  for (var i = event.resultIndex; i < event.results.length; i++) {
+    // check the callback
+    if (event.results[i].isFinal) {
+      console.log("Text received: " + event.results[i][0].transcript);
+      drawDirection(event.results[i][0].transcript);
+    }
+  }
+};
+
+recognition.onstop = function(event) {
+  console.log("Stopped listening.")
+};
+
+recognition.onstart = function(event) {
+  console.log("Started listening.");
+};
